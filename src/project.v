6942ba515a2c6385ac49d37d6e6327558f61fbb2
jinnosukeKato <jinnosukeKato@gmail.com>
2025-08-13 12:22:10 +0900

    とりあえずメモリのテストを書いた(落ちる)

--- src/project.v
+++ src/project.v
@@ -14,7 +14,7 @@ module tt_um_example (
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
-);
+  );
 
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
@@ -23,5 +23,27 @@ module tt_um_example (
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
+
+  wire [3:0] address = uio_in[7:4]; // メモリのアドレス
+  wire [3:0] opcode_in = ui_in[3:0]; // メモリへのオペコード入力
+  wire [3:0] immediate_in = uio_in[3:0]; // メモリへの即値入力
+  wire mem_write = ~ui_in[6]; // メモリへの書き込み信号（反転）
+
+  wire [3:0] opcode_out;// メモリからのオペコード出力
+  wire [3:0] immediate_out; // メモリからの即値出力
+
+  assign uo_out[3:0] = opcode_out;
+  assign uo_out[7:4] = immediate_out;
+
+  Memory memory(
+           .address(address),
+           .opcode_in(opcode_in),
+           .immediate_in(immediate_in),
+           .opcode_out(opcode_out),
+           .immediate_out(immediate_out),
+           .write(mem_write),
+           .clk(clk),
+           .rst_n(rst_n)
+         );
 
 endmodule
--- test/test.py
+++ test/test.py
@@ -26,15 +26,19 @@ async def test_project(dut):
     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+
+    # MSB ⇔ LSB
+    dut.uio_in.value = 0b0000_0101 # im=1010/addr=0000
+    dut.ui_in.value = 0b1000_0101 # opcode=1010/書き込みモード
 
     # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
 
+    dut.ui_in.value = 0b1010_1010 # opcode=0101←書き込まれない/読み込みモード
+
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    assert dut.uo_out.value == 0b0101_0101 # {opcode, im}
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
