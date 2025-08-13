45e99132bca01a4c800717fdee0c4088b7c44bcf
jun1okamura <jun1okamura@gmail.com>
2025-08-13 15:13:30 +0900

    #3

--- src/memory.v
+++ src/memory.v
@@ -14,32 +14,16 @@ module Memory (
 
   reg [7:0] data[0:15]; // 8bit 16wordメモリ
 
-  always @(posedge clk or negedge rst_n)
-  begin
-    if (!rst_n)
-    begin
+  always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
       // リセット(0クリア)
-      data[0] <= 8'b0;
-      data[1] <= 8'b0;
-      data[2] <= 8'b0;
-      data[3] <= 8'b0;
-      data[4] <= 8'b0;
-      data[5] <= 8'b0;
-      data[6] <= 8'b0;
-      data[7] <= 8'b0;
-      data[8] <= 8'b0;
-      data[9] <= 8'b0;
-      data[10] <= 8'b0;
-      data[11] <= 8'b0;
-      data[12] <= 8'b0;
-      data[13] <= 8'b0;
-      data[14] <= 8'b0;
-      data[15] <= 8'b0;
+      data <= 128'b0;
     end
-    else if (write)
-    begin
-      // 書き込み
-      data[address] <= {immediate_in, opcode_in};
+    else begin
+        if (write) begin
+          // 書き込み
+        data[address] <= {immediate_in, opcode_in};
+        end
     end
   end
 
